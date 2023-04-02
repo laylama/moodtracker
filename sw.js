ee24342d7cc8e317759299ff50e4421435605510
laylama <95572775+laylama@users.noreply.github.com>
2023-04-02 23:40:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
+const CACHE_VERSION = 'v2';
+const CACHE_NAME = `mood-tracker-cache-${CACHE_VERSION}`;
+
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('mood-tracker-cache').then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         'index.html',
         'scripts.js',
@@ -11,10 +15,30 @@ self.addEventListener('install', (event) => {
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName.startsWith('mood-tracker-cache-') && cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((fetchResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    })
+  );
+});
+
